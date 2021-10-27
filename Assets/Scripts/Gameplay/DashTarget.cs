2156c32dc0d631cca52d5cf90ddfe72c6e75a5a1
LucaBGt <32401333+LucaBGt@users.noreply.github.com>
2021-10-27 21:04:53 +0200

    + Added new Salt Sprites

--- Assets/Scripts/Gameplay/DashTarget.cs
+++ Assets/Scripts/Gameplay/DashTarget.cs
@@ -2,66 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using Pixelplacement;
 using UnityEngine;
-public class DashTarget : TickBase {
+public class DashTarget : TickBase
+{
 
-    [Header ("Settings")]
+    [Header("Settings")]
 
     public bool canDashToMe;
     public bool destroyAfterHit;
 
-    [Header ("References")]
+    [Header("References")]
     GameObject target;
     public ParticleSystem hit;
     public Collider2D actualCollider;
     public Player_Base player;
 
-    private void Awake () {
-        target = transform.GetChild (0).gameObject;
-        if(hit == null)
-        hit = GetComponent<ParticleSystem> ();
-        target.SetActive (false);
+    private void Awake()
+    {
+        target = transform.GetChild(0).gameObject;
+        if (hit == null)
+            hit = GetComponent<ParticleSystem>();
+        target.SetActive(false);
     }
-    private void OnTriggerEnter2D (Collider2D other) {
-        if (other.gameObject.CompareTag ("Player")) {
-            player = other.gameObject.GetComponent<Player_Base> ();
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject.GetComponent<Player_Base>();
             player.dashTarget = this.transform;
             canDashToMe = true;
         }
     }
 
-    public override void Tick () {
-        if (canDashToMe) {
+    public override void Tick()
+    {
+        if (canDashToMe)
+        {
 
-            if (player.isFacingObject (this.transform) || !player.mustFaceDash) {
-                target.SetActive (true);
-            } else {
-                target.SetActive (false);
+            if (player.isFacingObject(this.transform) || !player.mustFaceDash)
+            {
+                target.SetActive(true);
+            }
+            else
+            {
+                target.SetActive(false);
             }
 
-            if (!player.isDashing) {
+            if (!player.isDashing)
+            {
                 target.transform.right = player.transform.position - target.transform.position;
                 if (actualCollider != null)
                     actualCollider.enabled = true;
-            } else {
-                if (Vector3.Distance (player.transform.position, this.transform.position) < .3f) {
-                    hit.Play ();
-                    if(destroyAfterHit)
+            }
+            else
+            {
+                if (Vector3.Distance(player.transform.position, this.transform.position) < .3f)
+                {
+                    hit.Play();
+                    if (destroyAfterHit)
                         hit.transform.parent = null;
-                        Destroy (this.gameObject);
-                } else if (Vector3.Distance (player.transform.position, this.transform.position) < 1f) {
+                    Destroy(this.gameObject);
+                }
+                else if (Vector3.Distance(player.transform.position, this.transform.position) < 1f)
+                {
                     if (actualCollider != null)
                         actualCollider.enabled = false;
-                } else
-                if (actualCollider != null)
+                }
+                else
+              if (actualCollider != null)
                     actualCollider.enabled = true;
             }
-        } else {
-            target.SetActive (false);
+        }
+        else
+        {
+            target.SetActive(false);
         }
     }
 
-    private void OnTriggerExit2D (Collider2D other) {
-        if (other.gameObject.CompareTag ("Player")) {
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
             //player = other.gameObject.GetComponent<Player_Base>();
             player.dashTarget = null;
             player.isDashing = false;
